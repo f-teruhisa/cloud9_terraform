ebe7c5ddb4501485a88a107824a15dcbcf44858d
f-teruhisa <teru_fukumoto@outlook.jp>
2022-03-13 13:54:27 +0900

    Fix Cloud9 console URL

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "cloud9_access_url" {
   description = "Cloud9 Environment URL for access"
-  value       = "https://${var.region}/.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.cloud9_example.id}"
+  value       = "https://${var.region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.cloud9_example.id}"
 }
